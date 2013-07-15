96e8d1c8e5022bcaf8777853c8e5cccea9b4b806
Krzysztof Otrebski <otros.systems@gmail.com>
2013-07-15 17:51:49 +0200

    Apache2 copyright

--- src/main/java/com/negusoft/singleinstance/examples/ParameterPassing.java
+++ src/main/java/com/negusoft/singleinstance/examples/ParameterPassing.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2013 Krzysztof Otrebski (otros.systems@gmail.com)
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+
 package com.negusoft.singleinstance.examples;
 
 import com.negusoft.singleinstance.RequestDelegate;
@@ -68,7 +84,7 @@ public class ParameterPassing {
 			System.out.println("There is no instance currently running so we can go ahead:");
 			System.out.println("Doing some cool stuff, press ENTER key to stop...");
 			try {
-        int read = System.in.read();
+        System.in.read()
       } catch (IOException e) {
 				e.printStackTrace();
 			}
